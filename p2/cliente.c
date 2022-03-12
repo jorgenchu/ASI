16983d4716382233acda56f56f4edaddeb22ec23
jorgenchu <jorgenchu@gmail.com>
2022-03-12 09:46:47 +0000

    ejer con FIFO

--- p2/cliente.c
+++ p2/cliente.c
@@ -1,28 +1,33 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <string.h>
-#include <pthread.h>
 #include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define TAM 200
 
-
-
-
 int main(int argc, char const *argv[])
 {
-
-    
-    char mensaje [TAM];
+    int fde, fdl, n;
+    char buf[TAM];
+    char traducido[1024];
     printf("\nIntroduce un mensaje por pantalla:\n");
-    fflush(stdin);
+    fflush(stdin);    
+    fgets(buf, TAM, stdin);
     __fpurge(stdout);
-    fgets(mensaje, 200, TAM);
+/////ZONA DE ESCRITURA
+    mkfifo("/tmp/fifo", 0666);
+    fde = open("/tmp/fifo", O_WRONLY);
+    write(fde, buf, sizeof(buf));
+////ZONA DE LECTURA
+    fdl = open ("/tmp/fifo2", O_RDONLY);
+    n = read(fdl, traducido, sizeof(traducido));
+    printf("\nMensaje traducido: %s", traducido);   
+
+
+    close(fdl);
     
-
-
     return 0;
 }
--- p2/servidor.c
+++ p2/servidor.c
@@ -1,2 +1,38 @@
-lomismo
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <fcntl.h>
 
+
+#define TAM 200
+
+
+int main(int argc, char const *argv[])
+{
+    int fdl, fde, n;
+    char buf[1024];
+    
+/////LECTURA
+    fdl = open("/tmp/fifo", O_RDONLY);
+    n = read(fdl, buf, sizeof(buf));
+    char traducido[n];
+    printf("\n mensaje recibido: %s", buf);
+
+    for(int i = 0; i <n + 1;i++)
+    {
+       traducido[i] = toupper(buf[i]);
+    }
+    printf("\nTraducción: %s", traducido);
+    close(fdl);
+
+////ENVIO TRADUCIDO
+    mkfifo("/tmp/fifo2", 0666);
+    fde = open("/tmp/fifo2", O_WRONLY);
+    write(fde, traducido, sizeof(traducido));
+    close(fde);
+    
+    return 0;
+}
