6ea5b5265c8fe5613edbc6e2b6ea72d63f78e496
jorgenchu <jorgenchu@gmail.com>
2022-03-30 10:55:40 +0100

    ejercicio de colas

--- p2/ejer2_namedpipe/cliente.c
+++ p2/ejer2_namedpipe/cliente.c
@@ -1,1 +1,40 @@
-ajfokajfio
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/msg.h>
+
+#define CLAVE 0x72357116L
+
+typedef struct mensaje{
+    long canal;
+    char cad[200]; 
+}MENSAJE;
+
+int idCola;
+char resp;
+MENSAJE m;
+
+int main(int argc, char** argv) {
+
+    idCola = msgget(CLAVE,0666);
+    
+    do{
+       printf("Introduce el mensaje a traducir: ");
+       fflush(stdout);
+       __fpurge(stdin);
+       fgets(m.cad,200,stdin);
+       m.canal = 1L;
+       msgsnd(idCola,&m,sizeof(m)-sizeof(long),0);
+       
+       msgrcv(idCola,&m,sizeof(m)-sizeof(long),0,0);
+       printf("El mensaje traducido es: %s\n",m.cad);
+       fflush(stdout);
+       __fpurge(stdin);
+       printf("¿Quieres traducir mas mensajes? (s/n)\n");
+       fflush(stdout);
+       __fpurge(stdin);
+       scanf("%c",&resp);
+    }while(resp == 's');
+    
+    return (EXIT_SUCCESS);
+}
+
--- p2/ejer2_namedpipe/servidor.c
+++ p2/ejer2_namedpipe/servidor.c
@@ -8,8 +8,7 @@
 
 typedef struct mensaje{
     long canal;
-    int pidCliente;
-    char cad[200];
+    char cad[200]; 
 }MENSAJE;
 
 int idCola;
@@ -20,16 +19,15 @@ int main(int argc, char** argv) {
     idCola = msgget(CLAVE,IPC_CREAT|0666);
     
     while(1){
+        
         msgrcv(idCola,&m,sizeof(m)-sizeof(long),1,0);
-        printf("Cadena recibida: %s\n",m.cad);
         for(int i=0;i<strlen(m.cad);i++){
             m.cad[i] = toupper(m.cad[i]);
         }
-        
-        m.canal = m.pidCliente;
+    
+       
         msgsnd(idCola,&m,sizeof(m)-sizeof(long),0);
     }
     
     return (EXIT_SUCCESS);
 }
-
